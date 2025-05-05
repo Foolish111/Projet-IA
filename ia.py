c3f08dd530b6123236a164f38e58fe6ab5b746c4
Foolish111 <waliddtouil@gmail.com>
2025-05-05 18:47:44 +0200

    j'ai overwrite le fichier ia.py par erreur

--- ia.py
+++ ia.py
@@ -1,28 +1,26 @@
 import echec
 from echec import Echec
 import random
-import time
 
 class IA:
 
     def __init__(self, couleur, profondeur):
         self.couleur = couleur
         self.profondeur = profondeur
-        self.limite_temps = 3
-        self.debut_temps_coup = None
         self.valeurs = {
     "P": 1,  # Pion
-    "N": 3,  # Cavalier
-    "B": 3,  # Fou
-    "R": 5,  # Tour
-    "Q": 9,  # Reine
-    "K": 2  # Roi
+    "N": 3,  # Cavalier (Knight)
+    "B": 3,  # Fou (Bishop)
+    "R": 5,  # Tour (Rook)
+    "Q": 9,  # Reine (Queen)
+    "K": 2  # Roi (King) 
 }
-
+        self.table_transposition = {}
 
     def recup_mouv(self, jeu:Echec):
 
         mouv_dispo = jeu.tous_mouv_valides(self.couleur)
+        random.shuffle(mouv_dispo)
 
         if not mouv_dispo:
             return None
@@ -34,6 +32,7 @@ class IA:
         for mouv in mouv_dispo:
 
             jeu.faire_mouv(mouv)
+            self.table_transposition = {}
             #score = self.reverse_mini_max(jeu, self.profondeur, True, self.couleur)
             score = self.reverse_alpha_beta(jeu, self.profondeur, float('-inf'), float('+inf'), True, self.couleur)
             jeu.annuler_mouv()
@@ -69,34 +68,35 @@ class IA:
 
         # Cas de fin de partie
         if len(pieces_couleur) == 0:
-            return -1000  # On a perdu => donc on a gagné => favoriser ce cas
+            return 1000  # On a perdu → donc on a gagné → favoriser ce cas
         if len(pieces_adverses) == 0:
-            return 1000  # L'adversaire a perdu => on a perdu => mauvais
+            return -1000  # L'adversaire a perdu → on a perdu → mauvais
         if not coups_couleur:
             if len(pieces_couleur) < len(pieces_adverses):
-                return -1000  # Moins de pièces => gagne
+                return 1000  # Moins de pièces → gagne
             else:
-                return 1000  # Plus de pièces => perd
+                return -1000  # Plus de pièces → perd
         if not coups_adverses:
             if len(pieces_adverses) < len(pieces_couleur):
-                return 1000  # Adversaire gagne => on perd
+                return -1000  # Adversaire gagne → on perd
             else:
-                return -1000  # Il a plus de pièces => il perd => nous gagnons
+                return 1000  # Il a plus de pièces → il perd → nous gagnons
 
+        # Évaluation positionnelle
         def valeur_piece(p):
             valeurs = {"P": 1, "N": 2, "B": 2, "R": 3, "Q": 4, "K": 5}
             return valeurs[p.nom.upper()]
 
-        # nombre de nos pièces (on veut s'en débarrasser) - nombre de leurs pièces (on veut qu'ils en perdent)
+        # Score = nombre de nos pièces (on veut s'en débarrasser) - nombre de leurs pièces (on veut qu'ils en perdent)
         score_self = sum(valeur_piece(p) for p in pieces_couleur.values() if p)
         score_adv = sum(valeur_piece(p) for p in pieces_adverses.values() if p)
 
-        # on veut pouvoir bouger pour se faire capturer => bonus si beaucoup de coups possibles
+        # Mobilite : on veut pouvoir bouger pour se faire capturer → bonus si beaucoup de coups possibles
+        # En fait je pense qu'il faut limiter les coups, plus one se rapproche de 0 et plus on gagne
         mobilite_self = len(coups_couleur)
         mobilite_adv = len(coups_adverses)
 
-        # moins on a de points, mieux c'est
-        return (score_adv - score_self) + 0.1 * (mobilite_self - mobilite_adv)
+        return (score_adv - score_self) + 0.1 * (mobilite_adv - mobilite_self)
 
     def reverse_mini_max(self, jeu: Echec, profondeur, est_maximisant, couleur):
 
@@ -135,14 +135,11 @@ class IA:
 
             return meilleur_score
 
-    def reverse_alpha_beta(self, jeu: Echec, profondeur, alpha, beta, est_maximisant, couleur, table_transposition = None):
-
-        if table_transposition is None:
-            table_transposition = {}
+    def reverse_alpha_beta(self, jeu: Echec, profondeur, alpha, beta, est_maximisant, couleur):
 
         etat = jeu.recup_etat()
-        if etat in table_transposition:
-            score_stocke, profondeur_stockee, flag = table_transposition[etat]
+        if etat in self.table_transposition:
+            score_stocke, profondeur_stockee, flag = self.table_transposition[etat]
             if profondeur_stockee >= profondeur:
                 if flag == "exact":
                     return score_stocke
@@ -155,7 +152,7 @@ class IA:
 
         if jeu.partie_terminee() or profondeur == 0:
             score = self.heuristique(jeu, couleur)
-            table_transposition[etat] = (score, profondeur, "exact")
+            self.table_transposition[etat] = (score, profondeur, "exact")
             return score
 
         if est_maximisant:
@@ -175,7 +172,7 @@ class IA:
                 if beta <= alpha:
                     break
             flag = "exact" if meilleur_score == alpha else "borne_inf"
-            table_transposition[etat] = (meilleur_score, profondeur, flag)
+            self.table_transposition[etat] = (meilleur_score, profondeur, flag)
             return meilleur_score
 
         else:
@@ -188,12 +185,12 @@ class IA:
                     c = "noir"
                 else:
                     c = "blanc"
-                score = self.reverse_alpha_beta(jeu, profondeur - 1, alpha, beta, False, c)
+                score = self.reverse_alpha_beta(jeu, profondeur - 1, alpha, beta, True, c)
                 jeu.annuler_mouv()
                 meilleur_score = min(meilleur_score, score)
                 beta = min(beta, score)
                 if beta <= alpha:
                     break
             flag = "exact" if meilleur_score == beta else "borne_sup"
-            table_transposition[etat] = (meilleur_score, profondeur, flag)
+            self.table_transposition[etat] = (meilleur_score, profondeur, flag)
             return meilleur_score
